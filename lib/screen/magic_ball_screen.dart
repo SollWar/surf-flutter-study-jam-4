0f08aab5ea9bacff7109c3e03a1cd4017996c6ca
Denisov Nikita <sollwarmorgrind@gmail.com>
2023-07-25 14:57:14 +0300

    Анимация тени и текста

--- lib/screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen.dart
@@ -10,19 +10,30 @@ class MagicBallScreen extends StatefulWidget {
 }
 
 class _MagicBallTap extends State<MagicBallScreen> {
-
   PredictionData predictionData = PredictionData(reading: "");
   String prediction = "";
+  bool shadow = false;
+  bool textOpacity = true;
 
   void _setPrediction() {
     setState(() {
       prediction = predictionData.reading;
+      textOpacity = true;
     });
+  }
+
+  void _tapBall() {
+    setState(() {
+      shadow = true;
+      textOpacity = false;
+    });
+    _getData();
   }
 
   void _getData() async {
     predictionData = (await ApiService().getPrediction())!;
-    Future.delayed(const Duration(seconds: 1)).then((value) => _setPrediction());
+    Future.delayed(const Duration(milliseconds: 500))
+        .then((value) => _setPrediction());
   }
 
   @override
@@ -37,27 +48,45 @@ class _MagicBallTap extends State<MagicBallScreen> {
                     begin: Alignment.bottomCenter,
                     end: Alignment.topCenter,
                     colors: [
-                      Color.fromRGBO(0, 0, 2, 1),
-                      Color.fromRGBO(16, 12, 44, 1),
-                    ])),
+                  Color.fromRGBO(0, 0, 2, 1),
+                  Color.fromRGBO(16, 12, 44, 1),
+                ])),
           ),
           GestureDetector(
-            onTap: _getData,
-              child: Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(24.0),
-                child: Image.asset("image/ball_idle.png"),
-              )),
-          Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(24.0),
-              child: RichText(
-                text: TextSpan(
-                    text: prediction,
-                    style: const TextStyle(
-                        color: Colors.white, fontSize: 16)),
-                textAlign: TextAlign.center,
-              )),
+            onTap: _tapBall,
+            child: Stack(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(24.0),
+                  child: Image.asset("image/ball_idle.png"),
+                ),
+                AnimatedOpacity(
+                    opacity: shadow ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 500),
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(24.0),
+                      child: Image.asset("image/ball_shadow.png"),
+                    ),
+                ),
+                AnimatedOpacity(
+                    opacity: textOpacity ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 250),
+                  child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(24.0),
+                      child: RichText(
+                        text: TextSpan(
+                            text: prediction,
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 16)),
+                        textAlign: TextAlign.center,
+                      )),
+                )
+              ],
+            ),
+          ),
           Container(
             alignment: Alignment.bottomCenter,
             padding: const EdgeInsets.all(65.0),
